ccd9f996d94f3dcfc85c34f1cb2d333f15b9d5a0
feng tiantian <HorizonFTT@outlook.com>
2020-01-12 17:41:31 +0800

    update

--- block.js
+++ block.js
@@ -1,15 +1,19 @@
-var Block = function () {
+var Block = function (position) {
     var image = imageFromPath('block.png')
     var o = {
         image: image,
-        x: 100,
-        y: 100,
+        x: position[0],
+        y: position[1],
         w: 50,
         h: 20,
         alive: true,
+        life:position[2] || 1
     }
     o.kill = function () {
-        o.alive = false
+        --o.life
+        if(o.life==0){
+            o.alive = false
+        }
     }
     o.collide = function (ball) {
         return o.alive && (rectIntersects(o, ball) || rectIntersects(ball, o))
