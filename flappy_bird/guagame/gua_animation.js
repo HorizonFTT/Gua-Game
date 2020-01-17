6514e28e09215600cc18812a89e4557ddb885568
feng tiantian <HorizonFTT@outlook.com>
2020-01-18 01:06:28 +0800

    update

--- flappy_bird/guagame/gua_animation.js
+++ flappy_bird/guagame/gua_animation.js
@@ -17,6 +17,7 @@ class GuaAnimation {
         this.frameCount = 3
 
         this.flipX = false
+        this.rotation = 0
 
         this.gy = 10
         this.vy = 0
@@ -29,6 +30,7 @@ class GuaAnimation {
     }
     jump() {
         this.vy = -10
+        this.rotation = -45
     }
     update() {
         this.y += this.vy
@@ -36,6 +38,10 @@ class GuaAnimation {
         var h = 515
         if (this.y > h) {
             this.y = h
+        }
+
+        if (this.rotation < 45) {
+            this.rotation += 5
         }
 
         this.frameCount -= 1
@@ -47,19 +53,20 @@ class GuaAnimation {
     }
     draw() {
         var context = this.game.context
+        context.save()
+
+        var w2 = this.w / 2
+        var h2 = this.h / 2
+        context.translate(this.x + w2, this.y + h2)
         if (this.flipX) {
-            context.save()
-
-            var x = this.x + this.w / 2
-            context.translate(x, 0)
             context.scale(-1, 1)
-            context.translate(-x, 0)
-            context.drawImage(this.texture.image, this.x, this.y)
-
-            context.restore()
-        } else {
-            context.drawImage(this.texture.image, this.x, this.y)
         }
+        context.rotate(this.rotation * Math.PI / 180)
+        context.translate(-w2, -h2)
+
+        context.drawImage(this.texture.image, 0, 0)
+
+        context.restore()
     }
     move(x, keyStatus) {
         this.flipX = (x < 0)
